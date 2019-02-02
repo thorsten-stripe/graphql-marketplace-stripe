0f7feac1cf826af11bca607cc0ce63b48946b09c
Thorsten Schaeff <thorsten@stripe.com>
2019-02-02 18:30:42 +0100

    signup customer and account

--- src/resolvers/Mutations.js
+++ src/resolvers/Mutations.js
@@ -1,3 +1,44 @@
-const Mutations = {};
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const Mutations = {
+  async signup(parent, args, ctx, info) {
+    const { email, name } = args;
+    // Create Stripe Customer object.
+    const customer = await stripe.customers.create({
+      description: `Customer for ${name}`,
+      email
+    });
+
+    // Create Stripe Account object.
+    const account = await stripe.accounts.create({
+      type: "custom",
+      email
+    });
+
+    // Create the user in the database.
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          name,
+          email,
+          customer: {
+            create: {
+              email,
+              stripeId: customer.id
+            }
+          },
+          account: {
+            create: {
+              email,
+              stripeAccountId: account.id
+            }
+          }
+        }
+      },
+      info
+    );
+    return user;
+  }
+};
 
 module.exports = Mutations;
